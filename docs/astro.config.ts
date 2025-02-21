8bd63c0df068be34a1394b3219f22817b0dd0ce7
Chris Swithinbank <swithinbank@gmail.com>
2025-02-21 01:06:17 +0100

    Docs tweaks

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -1,24 +1,25 @@
-import starlight from '@astrojs/starlight'
-import { defineConfig } from 'astro/config'
-import starlightThemeFlexoki from 'starlight-theme-flexoki'
+import starlight from '@astrojs/starlight';
+import { defineConfig } from 'astro/config';
+import starlightThemeFlexoki from 'starlight-theme-flexoki';
 
 export default defineConfig({
-  integrations: [
-    starlight({
-      editLink: {
-        baseUrl: 'https://github.com/delucis/starlight-theme-flexoki/edit/main/docs/',
-      },
-      plugins: [starlightThemeFlexoki()],
-      sidebar: [
-        {
-          label: 'Start Here',
-          items: [{ slug: 'getting-started' }],
-        },
-      ],
-      social: {
-        github: 'https://github.com/delucis/starlight-theme-flexoki',
-      },
-      title: 'starlight-theme-flexoki',
-    }),
-  ],
-})
+	integrations: [
+		starlight({
+			title: 'Starlight Flexoki',
+			social: {
+				github: 'https://github.com/delucis/starlight-theme-flexoki',
+			},
+			editLink: {
+				baseUrl: 'https://github.com/delucis/starlight-theme-flexoki/edit/main/docs/',
+			},
+			plugins: [starlightThemeFlexoki()],
+			sidebar: [
+				{
+					label: 'Start Here',
+					items: ['getting-started', 'demo'],
+				},
+				{ label: 'Examples', autogenerate: { directory: 'examples' } },
+			],
+		}),
+	],
+});
