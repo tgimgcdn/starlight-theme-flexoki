c42192f0d11a190befb5290996486a932c318824
Chris Swithinbank <swithinbank@gmail.com>
2025-02-21 01:49:20 +0100

    First styling pass

--- packages/starlight-theme-flexoki/index.ts
+++ packages/starlight-theme-flexoki/index.ts
@@ -1,21 +1,120 @@
-import type { StarlightPlugin } from '@astrojs/starlight/types'
+import type { StarlightPlugin, StarlightUserConfig } from '@astrojs/starlight/types';
+import { fileURLToPath } from 'node:url';
+import darkTheme from './syntax-themes/flexoki-dark.json';
+import lightTheme from './syntax-themes/flexoki-light.json';
+import type { AstroIntegration } from 'astro';
 
 export default function starlightThemeFlexoki(): StarlightPlugin {
-  return {
-    name: 'starlight-theme-flexoki',
-    hooks: {
-      setup({ logger }) {
-        /**
-         * This is the entry point of your Starlight plugin.
-         * The `setup` hook is called when Starlight is initialized (during the Astro `astro:config:setup` integration
-         * hook).
-         * To learn more about the Starlight plugin API and all available options in this hook, check the Starlight
-         * plugins reference.
-         *
-         * @see https://starlight.astro.build/reference/plugins/
-         */
-        logger.info('Hello from the starlight-theme-flexoki plugin!')
-      },
-    },
-  }
+	return {
+		name: 'starlight-theme-flexoki',
+		hooks: {
+			setup({ config, updateConfig, addIntegration }) {
+				// Register the theme’s custom CSS, including any user CSS *after* our own.
+				const newConfig: Partial<StarlightUserConfig> = {
+					customCss: ['starlight-theme-flexoki/styles.css', ...(config.customCss || [])],
+				};
+
+				// As long as the user hasn’t disabled Expressive Code, apply our styles.
+				if (config.expressiveCode !== false) {
+					const userExpressiveCodeConfig =
+						!config.expressiveCode || config.expressiveCode === true ? {} : config.expressiveCode;
+
+					newConfig.expressiveCode = {
+						// Syntax highlighting themes.
+						themes: [lightTheme, darkTheme],
+
+						...userExpressiveCodeConfig,
+
+						styleOverrides: {
+							// Basic frame styles
+							codeBackground: ['#100f0f', '#fffcf0'],
+							borderRadius: '0.25rem',
+							borderColor: ['var(--sl-color-gray-6)', 'var(--sl-color-gray-7)'],
+							borderWidth: '0.1875rem',
+							uiFontSize: 'var(--sl-text-sm)',
+							codeFontSize: 'var(--sl-text-code)',
+
+							// Reduce padding slightly to counteract thicker borders.
+							uiPaddingBlock: '0.15625rem' /** 2.5px */,
+
+							...userExpressiveCodeConfig.styleOverrides,
+
+							frames: {
+								frameBoxShadowCssValue: 'none',
+								// Terminal
+								terminalTitlebarBorderBottomColor: 'transparent',
+								terminalTitlebarBackground: 'var(--sl-color-gray-7, var(--sl-color-gray-6))',
+								// Editor frame
+								editorTabBarBackground: 'var(--sl-color-gray-7, var(--sl-color-gray-6))',
+								editorActiveTabIndicatorTopColor: 'var(--sl-color-accent)',
+								editorActiveTabIndicatorHeight: '2px',
+								// Copy button
+								inlineButtonBackground: 'var(--sl-color-gray-4)',
+								inlineButtonBorderOpacity: '0',
+								tooltipSuccessBackground: 'var(--sl-color-text-accent)',
+								tooltipSuccessForeground: 'var(--sl-color-text-invert)',
+
+								...userExpressiveCodeConfig.styleOverrides?.frames,
+							},
+
+							textMarkers: {
+								inlineMarkerBorderWidth: '1px',
+								lineMarkerAccentWidth: '0.1875rem',
+
+								// red-900 / red-100
+								delBackground: ['#3e1715', '#ffcabb'],
+								// red-400 / red-600
+								delBorderColor: ['#d14d41', '#af3029'],
+								delDiffIndicatorColor: ['#d14d41', '#af3029'],
+
+								// green-900 / green-100
+								insBackground: ['#252d09', '#dde2b2'],
+								// green-400 / green-600
+								insBorderColor: ['#879a39', '#66800b'],
+								insDiffIndicatorColor: ['#879a39', '#66800b'],
+
+								// blue-900 / blue-50
+								markBackground: ['#12253b', '#e1eceb'],
+								// blue-400 / blue-600
+								markBorderColor: ['#4385be', '#205ea6'],
+
+								...userExpressiveCodeConfig.styleOverrides?.textMarkers,
+							},
+						},
+					};
+				}
+				updateConfig(newConfig);
+
+				addIntegration(ViteOverridesIntegration());
+			},
+		},
+	};
+}
+
+/**
+ * Configure’s Vite to import our `<ContentNotice>` override instead of Starlight’s.
+ * This is used by Starlight’s `<FallbackContentNotice>` and `<DraftContentNotice>` components.
+ */
+function ViteOverridesIntegration() {
+	return {
+		name: 'starlight-theme-flexoki',
+		hooks: {
+			'astro:config:setup'({ updateConfig }) {
+				updateConfig({
+					vite: {
+						resolve: {
+							alias: [
+								{
+									find: /^\.\/ContentNotice\.astro$/,
+									replacement: fileURLToPath(
+										new URL('./overrides/ContentNotice.astro', import.meta.url)
+									),
+								},
+							],
+						},
+					},
+				});
+			},
+		},
+	} satisfies AstroIntegration;
 }
