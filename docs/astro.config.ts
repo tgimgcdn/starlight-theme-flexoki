51b77ead42a0c5b00d5f996cf3ce9f1b4f8df792
Chris Swithinbank <swithinbank@gmail.com>
2025-02-21 01:54:25 +0100

    Add GitHub `site` and `base`

--- docs/astro.config.ts
+++ docs/astro.config.ts
@@ -3,9 +3,12 @@ import { defineConfig } from 'astro/config';
 import starlightThemeFlexoki from 'starlight-theme-flexoki';
 
 export default defineConfig({
+	site: 'https://delucis.github.io',
+	base: '/starlight-theme-flexoki',
 	integrations: [
 		starlight({
 			title: 'Starlight Flexoki',
+			description: 'Starlight theme based on the Flexoki colour palette',
 			social: {
 				github: 'https://github.com/delucis/starlight-theme-flexoki',
 			},
